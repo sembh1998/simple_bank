6b67bf561c01e42d5173a83f07256ab0a6802e9d
sem benavente <sembenavente@gmail.com>
2024-07-22 00:59:29 -0500

    test: create account repository

---
+++ util/random.go
@@ -0,0 +1,46 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+
+}
+
+// generates a random integer between min and max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// generates a random string of length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+// generates a random owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// generates a random amount of money
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+// generates a random currency code
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "CAD", "PEN"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
